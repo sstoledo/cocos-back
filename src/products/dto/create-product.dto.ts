52857a79df10c1b816251353bd7b0a57d530bb3c
Adrian Toledo <toledosandro59@gmail.com>
2026-07-08 11:11:02 -0500

    feat(products): add CreateProductDto and UpdateProductDto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,48 @@
-export class CreateProductDto {}
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
+  code: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(500)
+  description?: string;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  @Type(() => Number)
+  price: number;
+
+  @IsInt()
+  @Min(0)
+  @Type(() => Number)
+  minStock: number;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  unit?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+}
