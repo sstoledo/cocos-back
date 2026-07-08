fb5c62684842c7bea633a2dc8c58a04a520e0c26
Adrian Toledo <toledosandro59@gmail.com>
2026-07-08 09:58:54 -0500

    style(upload): sort cloudinary imports

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { v2 as cloudinary, UploadApiResponse } from 'cloudinary';
+import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
 
 export interface UploadedImage {
   url: string;
